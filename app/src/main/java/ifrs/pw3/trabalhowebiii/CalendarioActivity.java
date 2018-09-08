47fc17b639fe7e3a7ed78c127294e27c8a0de081
Patrick Garcia <patrick.garcia@aluno.poa.ifrs.edu.br>
2018-09-08 19:33:11 -0300

    Adicionando activities

---
+++ app/src/main/java/ifrs/pw3/trabalhowebiii/CalendarioActivity.java
@@ -0,0 +1,13 @@
+package ifrs.pw3.trabalhowebiii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalendarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendario);
+    }
+}
---
+++ app/src/main/java/ifrs/pw3/trabalhowebiii/GerenciarPerfilActivity.java
@@ -0,0 +1,13 @@
+package ifrs.pw3.trabalhowebiii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GerenciarPerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerenciar_perfil);
+    }
+}
