83bd4c0dd108e92fb5cad5ca49055f760f9564f6
Patrick Garcia <patrick.garcia@aluno.poa.ifrs.edu.br>
2018-09-08 21:55:45 -0300

    Add classe Pessoa e atualizando tela de login

---
+++ app/src/main/java/ifrs/pw3/trabalhowebiii/Pessoa.java
@@ -0,0 +1,76 @@
+package ifrs.pw3.trabalhowebiii;
+
+import java.util.Objects;
+
+public class Pessoa {
+    private Integer id;
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa(Integer id, String nome, String endereco, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pessoa)) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(getId(), pessoa.getId()) &&
+                Objects.equals(getNome(), pessoa.getNome()) &&
+                Objects.equals(getEndereco(), pessoa.getEndereco()) &&
+                Objects.equals(getTelefone(), pessoa.getTelefone());
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(getId(), getNome(), getEndereco(), getTelefone());
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", endereco='" + endereco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
+}
