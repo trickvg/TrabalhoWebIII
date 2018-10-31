02eadf3a91fd1a89120e6399d11e825acf8d891e
Patrick Garcia <patrick.garcia@aluno.poa.ifrs.edu.br>
2018-10-31 02:54:46 -0300

    Adicionando Firebase e depedências

---
+++ app/src/main/java/ifrs/pw3/trabalhowebiii/model/Produto.java
@@ -0,0 +1,61 @@
+package ifrs.pw3.trabalhowebiii.model;
+
+public class Produto {
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double valorUnitario;
+
+    public Produto(){}
+    public Produto(String nome, String descricao, Double valorUnitario) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+    }
+    public Produto(String id, String nome, String descricao, Double valorUnitario) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valorUnitario = valorUnitario;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(Double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", valorUnitario=" + valorUnitario +
+                '}';
+    }
+}
--- app/src/main/java/ifrs/pw3/trabalhowebiii/view/MainActivity.java
+++ app/src/main/java/ifrs/pw3/trabalhowebiii/view/MainActivity.java
@@ -1,21 +1,31 @@
 package ifrs.pw3.trabalhowebiii.view;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import ifrs.pw3.trabalhowebiii.R;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText edtEmail;
+    private EditText edtSenha;
+    private Button btnLogin;
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtEmail = findViewById(R.id.edicaoNome);
+        edtSenha = findViewById(R.id.edicaoSenha);
+        btnLogin = findViewById(R.id.btnLoginEntrar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
 
 
         Button botaoEntrar = findViewById(R.id.btnLoginEntrar);
